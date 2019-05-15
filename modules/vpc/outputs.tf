448b275ee937ddd7412a6e54bf355dcbc8af6809
Felipe Gomes <felipgomesilva@gmail.com>
2019-05-15 11:37:47 -0300

    add more options in outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,14 @@ output "vpc_id" {
   value = "${aws_vpc.generic_vpc.id}"
 }
 
-output "subnet_id" {
+output "subnet_pub" {
   value = "${aws_subnet.generic_pub_subnet.*.id}"
+}
+
+output "subnet_pvt" {
+  value = "${aws_subnet.generic_pvt_subnet.*.id}"
+}
+
+output "subnet_db" {
+  value = "${aws_subnet.generic_db_pvt_subnet.*.id}"
 }
